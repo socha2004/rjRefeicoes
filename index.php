888d345e8f169c711fa019e0c3374616714854e1
eugenio socha <eugenio.socha2004@gmail.com>
2024-09-16 18:05:25 -0400

    Deifinida estrutura do html

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>RJ Refeições</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <head>
+        <div class="container">
+            <img src="#" alt="Logo">
+            <h1>Rj Refeicoes</h1>
+            <span>Aberto para pedidos.</span>
+        </div>
+    </head>
+    <main>
+        <div class="cardapio-area container">
+            <h2>Pratos do dia</h2>
+            <div class="card">
+                Refeição 1
+            </div>
+            <div class="card">
+                Refeição 2
+            </div>
+            <div class="card">
+                Refeição 3
+            </div>
+        </div>
+    </main>
+    <footer class="rodape-area">
+        <h3>&copy;Seven Code</h3>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+</html>
