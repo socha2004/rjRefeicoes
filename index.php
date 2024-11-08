874bb583910d3147d5d9379eb6eba03f60caf8e4
Eugenio - PC Casa <eugenio.socha2004@gmail.com>
2024-11-07 22:32:46 -0400

    add modal em cards

--- index.php
+++ index.php
@@ -14,8 +14,11 @@ session_start();
     <link rel="stylesheet" href="src/styles/estiloIndex/estilo.css" type="text/css">
     <link rel="stylesheet" href="src/styles/menuIndex/estilo.css">
     <script src="https://kit.fontawesome.com/4a5dfc2a50.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
     <style>
-        .cabecalho{
+        .cabecalho {
             background-image: url('src/assets/imagens/FOGO.jpg');
             background-size: cover;
             background-position: center;
@@ -28,7 +31,7 @@ session_start();
 
 <body>
     <header class="cabecalho">
-        <div class="menu-topo d-flex justify-content-between p-2">
+        <nav class="menu-topo d-flex justify-content-between p-2">
             <label class="popup">
                 <input type="checkbox">
                 <div class="burger" tabindex="0">
@@ -120,7 +123,7 @@ session_start();
                 </nav>
             </label>
             <img src="src/assets/imagens/cart.svg" alt="notification-atalho" width="50px">
-        </div>
+        </nav>
         <div class="d-flex align-items-center p-2 cabecalho-container">
             <img src="src/assets/imagens/logo.png" alt="Logo" class="logo">
             <div class="d-flex flex-column">
@@ -131,21 +134,20 @@ session_start();
         </div>
     </header>
     <main>
-        <div class="cardapio-area">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-area">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-item nav-link" href="#">Pratos do Dia</a>
-                        <a class="nav-item nav-link" href="#">Sobremesa</a>
-                        <a class="nav-item nav-link" href="#">Refrigerantes</a>
-                        <a class="nav-item nav-link" href="#">Sucos</a>
-                    </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light p-1">
+            <a class="navbar-brand" href="#">Menu de Refeições</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="#">Pratos do Dia</a>
+                    <a class="nav-item nav-link" href="#">Sobremesa</a>
+                    <a class="nav-item nav-link" href="#">Refrigerantes</a>
+                    <a class="nav-item nav-link" href="#">Sucos</a>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
         <div class="container-md menu-area wrap d-flex justify-content-around">
             <!--ENVOLVER OS CARDS AQUI!!!-->
             <?php
@@ -156,14 +158,34 @@ session_start();
 
             if ($resultado->num_rows > 0) {
                 while ($linha = $resultado->fetch_assoc()) {
+                    $modalId = "detalhesPratoModal" . $linha['id_produto'];
                     echo "<div class='card' style='width: 18rem;'>
                             <img class='card-img-top' src='$linha[imagem_produto]' alt='Card image cap'>
                             <h5 class='card-title p-1'>$linha[nome_produto]</h5>
                             <div class='card-body'>
                                 <p class='card-text'>$linha[descricao_produto]</p>
                                 <p class='card-text'><b>Preço:</b> $linha[preco_produto]</p>
-                                <button type='button' class='btn btn-primary'>Comprar</button>
+                                <button type='button' class='btn btn-success'>Comprar</button>
+                                <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#$modalId'>Detalhes</button>
                             </div>
+                        </div>
+                        <div class='modal fade' id='$modalId' tabindex='-1' aria-labelledby=''{$modalId}Label' aria-hidden='true'>
+                            <div class='modal-dialog modal-dialog-centered'>
+                                <div class='modal-content'>
+                                    <div class='modal-header'>
+                                        <h5 class='modal-title' id='{$modalId}Label'>$linha[nome_produto]</h5>
+                                        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Fechar'></button>
+                                    </div>
+                                    <div class='modal-body'>
+                                        <!-- Conteúdo detalhado do prato -->
+                                        <p>Descrição: $linha[descricao_produto]</p>
+                                        <p>Preço: $linha[preco_produto]</p>
+                                    </div>
+                                <div class='modal-footer'>
+                                    <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Fechar</button>
+                                </div>
+                            </div>
+                        </div>
                         </div>";
                 }
             } else {
@@ -175,9 +197,6 @@ session_start();
 
         </div>
     </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
